8721dd9f014e2cce2cad2e076b62b92629baaea3
Zeha Irawan <zehairawan@gmail.com>
2022-01-20 17:14:00 +0800

    implement upload feature

---
+++ src/base.ts
@@ -0,0 +1,13 @@
+import { getStorage } from '@firebase/storage';
+import { initializeApp } from 'firebase/app';
+
+export const app = initializeApp({
+  projectId: process.env.REACT_APP_projectId,
+  appId: process.env.REACT_APP_appId,
+  storageBucket: process.env.REACT_APP_storageBucker,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+});
+
+export const storage = getStorage(app);
