59064179e075773e8eef74124d888e6b19775649
Zeha Irawan <zehairawan@gmail.com>
2022-01-25 20:43:32 +0800

    display captions

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 // eslint-disable-next-line import/extensions
-import { BigPlayButton, Player, ControlBar } from 'video-react';
+import {
+  BigPlayButton, ClosedCaptionButton, ControlBar, Player,
+} from 'video-react';
 import 'video-react/dist/video-react.css';
 import video from '../videos/airline.mp4';
+// import subtitle from '../videos/airline.srt'
 
 function Display() {
   return (
     <div>
       <h1>Display</h1>
       <div className="w-1/2">
-        <Player playsInline>
+        <Player
+          playsInline
+        >
           <BigPlayButton position="center" className="hidden" />
           <source src={video} />
-          <ControlBar autoHide={false} />
+
+          <track
+            kind="captions"
+            src="/airline.vtt"
+            srcLang="en"
+            label="English"
+            default
+          />
+          <ControlBar autoHide={false}>
+            <ClosedCaptionButton order={7} />
+          </ControlBar>
         </Player>
       </div>
     </div>
