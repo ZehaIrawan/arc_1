bc09cfeefa46577bb44c4ae67922decbf3396880
Zeha Irawan <zehairawan@gmail.com>
2022-01-25 16:57:43 +0800

    separate generateSRT

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 
 const core = require('./core');
 const cleanup = require('./cleanup');
+const generateSRT = require('./generateSRT');
 exports.core = core.core;
 exports.cleanup = cleanup.cleanup;
+exports.generateSRT = generateSRT.generateSRT;
