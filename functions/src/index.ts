76c1191b855c59494480107320bb23de9aa7f17e
Zeha Irawan <zehairawan@gmail.com>
2022-01-27 07:29:04 +0800

    use nextjs instead of react

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-
-const core = require('./core');
-const cleanup = require('./cleanup');
-const generateSRT = require('./generateSRT');
-exports.core = core.core;
-exports.cleanup = cleanup.cleanup;
-exports.generateSRT = generateSRT.generateSRT;
